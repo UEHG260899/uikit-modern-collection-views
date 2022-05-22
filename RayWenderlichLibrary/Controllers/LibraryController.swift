4de2900d835a5bc88c8f081db72d77bf5d956ce3
Uriel Enrique <urielenrique99@gmail.com>
2022-05-21 19:24:09 -0500

    initial configuration of the collectionView

--- RayWenderlichLibrary/Controllers/LibraryController.swift
+++ RayWenderlichLibrary/Controllers/LibraryController.swift
@@ -11,5 +11,31 @@ final class LibraryController: UIViewController {
   
   private func setupView() {
     self.title = "Library"
+      collectionView.collectionViewLayout = configureCollectionViewLaout()
   }
+}
+
+// MARK: - CollectionView -
+
+extension LibraryController {
+    func configureCollectionViewLaout() -> UICollectionViewCompositionalLayout {
+        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.6), heightDimension: .fractionalHeight(0.3))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            section.interGroupSpacing = 10
+            
+            return section
+        }
+        
+        return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
+    }
 }
